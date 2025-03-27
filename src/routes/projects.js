222dc39bd238f0fc4e2d113eff96f840c5e2fc0a
Kendra Dias <kendra.dias@hotmail.com>
2025-03-27 14:38:13 -0700

    updated projects route to include auth middleware

--- src/routes/projects.js
+++ src/routes/projects.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const projectController = require('../../controllers/projectController');
+const { ensureAdmin } = require('../../config/auth');
 const upload = require('../../config/multer');
 
 // Get all projects
@@ -9,23 +10,25 @@ router.get('/', projectController.getAllProjects);
 // Search projects
 router.get('/search', projectController.searchProjects);
 
+
+// Admin ONLY routes
 // Show form to create a new project
-router.get('/create', projectController.showCreateProjectForm);
+router.get('/create', ensureAdmin, projectController.showCreateProjectForm);
 
 // Create a new project
-router.post('/', upload.single('screenshot'), projectController.createProject);
+router.post('/', ensureAdmin, upload.single('screenshot'), projectController.createProject);
 
 // Show form to edit a project
-router.get('/:id/edit', projectController.showEditProjectForm);
+router.get('/:id/edit', ensureAdmin, projectController.showEditProjectForm);
 
 // Update a project
-router.post('/:id/update', upload.single('screenshot'), projectController.updateProject);
+router.post('/:id/update', ensureAdmin, upload.single('screenshot'), projectController.updateProject);
 
 // Show delete confirmation page
-router.get('/:id/delete', projectController.showDeleteConfirmation);
+router.get('/:id/delete', ensureAdmin, projectController.showDeleteConfirmation);
 
 // Delete a project
-router.post('/:id/delete', projectController.deleteProject);
+router.post('/:id/delete', ensureAdmin, projectController.deleteProject);
 
 // Get project by ID (should be last to avoid conflicts with other routes)
 router.get('/:id', projectController.getProjectById);
