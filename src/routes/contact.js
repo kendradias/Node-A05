89392e3ff8392e8dda5ca9dc35217b21936c51ac
Kendra Dias <kendra.dias@hotmail.com>
2025-03-27 14:40:20 -0700

    updated contact route to include auth

--- src/routes/contact.js
+++ src/routes/contact.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const contactController = require('../../controllers/contactController');
+const {ensureAuthenticated} = require('../../config/auth');
 
 // Get contact page
-router.get('/', contactController.getContactPage);
+router.get('/', ensureAuthenticated, contactController.getContactPage);
 
 // Submit contact form
-router.post('/', contactController.submitContactForm);
+router.post('/', ensureAuthenticated, contactController.submitContactForm);
 
 module.exports = router;
